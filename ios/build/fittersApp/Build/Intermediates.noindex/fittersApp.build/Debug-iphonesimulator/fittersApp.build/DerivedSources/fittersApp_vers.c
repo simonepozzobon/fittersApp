cae107a488be27522b8592785a3df7089d66a26a
Simone <info@simonepozzobon.com>
2020-01-23 22:21:44 +0100

    update

--- ios/build/fittersApp/Build/Intermediates.noindex/fittersApp.build/Debug-iphonesimulator/fittersApp.build/DerivedSources/fittersApp_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char fittersAppVersionString[];
- extern const double fittersAppVersionNumber;
-
- const unsigned char fittersAppVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:fittersApp  PROJECT:fittersApp-1" "\n";
- const double fittersAppVersionNumber __attribute__ ((used)) = (double)1.;
--- ios/build/fittersApp/Build/Intermediates.noindex/fittersApp.build/Debug-iphonesimulator/fittersApp.build/Objects-normal/x86_64/fittersApp_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/esse/fittersApp/ios/build/fittersApp/Build/Intermediates.noindex/fittersApp.build/Debug-iphonesimulator/fittersApp.build/DerivedSources/fittersApp_vers.c
