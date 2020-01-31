f8b2519b1f55601eaf2e6e6d3ae0d6ba44ea4adc
Simone <info@simonepozzobon.com>
2020-01-31 21:36:10 +0100

    PageTitle clean

---
+++ src/components/UiInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class UiInput extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default UiInput
--- src/components/UiPageTitle.js
+++ src/components/UiPageTitle.js
@@ -1,46 +1,25 @@
-import React, {
-    Component
-}
-from 'react'
-import {
-    StyleSheet,
-    Text,
-    View,
-    Dimensions,
-}
-from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, Text } from "react-native";
 
 class UiPageTitle extends Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
+	constructor() {
+		super();
+		this.state = {};
+	}
 
-    // Component State Management
-    componentDidMount() {}
-
-    // Methods
-
-    // Render
-    render() {
-        // Dynamic styles
-        const compStyles = StyleSheet.create({})
-
-        // Component
-        return (
-            <Text style={styles.pageTitle}>
-              {this.props.title}
-            </Text>
-        );
-    }
+	// Render
+	render() {
+		// Component
+		return <Text style={styles.pageTitle}>{this.props.title}</Text>;
+	}
 }
 
 const styles = StyleSheet.create({
-    pageTitle: {
-        fontSize: 16,
-        fontWeight: '700',
-        color: '#FC2D1C',
-    }
-})
+	pageTitle: {
+		fontSize: 16,
+		fontWeight: "700",
+		color: "#FC2D1C"
+	}
+});
 
 export default UiPageTitle;
